040b6f011032611534442a595784ca97fc77e5d1
Paul Henschel <drcmda@gmail.com>
2015-12-29 18:14:49 +0100

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,112 @@
+var gulp = require('gulp-param')(require('gulp'), process.argv),
+    plugins = require('gulp-load-plugins')(),
+    merge = require('merge-stream'),
+    nodePath = '!node_modules/**',
+    bowerPath = '!bower_components/**';
+
+gulp.task('serve', function(live, build, open) {
+    var server = plugins.liveServer.static(build ? './build' : '.', 8080);
+    if (!live) server.config.livereload = false;
+    server.start();
+
+    if (live) {
+        gulp.watch(['*.{js,css,html}', '**/*.{js,css,html,txt,json}', nodePath, bowerPath], function() {
+            server.notify.apply(server, arguments);
+        });
+    }
+
+    if (open) {
+        gulp.src(__filename)
+            .pipe(plugins.open({
+                uri: 'http://localhost:8080/index.html'
+            }));
+    }
+});
+
+gulp.task('img', function(force) {
+    var pngquant = require('imagemin-pngquant');
+    var imageminJpegRecompress = require('imagemin-jpeg-recompress');
+    return gulp.src(['*.{jpg,png,gif}', 'images/**/*.{jpg,png,gif}'], {
+            base: './'
+        })
+        .pipe(plugins.if(!force, plugins.changed('build')))
+        .pipe(plugins.imagemin({
+            progressive: true,
+            optimizationLevel: 5,
+            use: [pngquant({quality: '50-75'}), imageminJpegRecompress({loops: 3, quality: 'medium'})]
+        }))
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('copy', function(force) {
+    return gulp.src(['*.md', 'fonts/*', nodePath, bowerPath], {
+            base: './'
+        })
+        /*.pipe(plugins.if(!force, plugins.changed('build')))*/
+        .pipe(gulp.dest('build'))
+});
+
+gulp.task('html', function(force) {
+    return gulp.src(['*.html', '**/*.html', nodePath, bowerPath], {
+            base: './'
+        })
+        /*.pipe(plugins.if(!force, plugins.changed('build')))*/
+        /*.pipe(plugins.inlineSource())*/
+        .pipe(gulp.dest('build'))
+});
+
+gulp.task('js', function(force) {
+    return gulp.src(['*.js', '!gulpfile.js'], {
+            base: './'
+        })
+        .pipe(plugins.if(!force, plugins.changed('build')))
+        .pipe(plugins.uglify())
+        .pipe(gulp.dest('build'))
+});
+
+gulp.task('css', function(force) {
+    return gulp.src(['*.css', '**/*.css', nodePath, bowerPath], {
+            base: './'
+        })
+        /*.pipe(plugins.if(!force, plugins.changed('build')))*/
+        .pipe(plugins.autoprefixer())
+        .pipe(plugins.csso())
+        .pipe(gulp.dest('build'));
+});
+
+/*gulp.task('critical', ['default'], function() {
+    var fs = require('fs');
+    var penthouse = require('penthouse');
+    var cave = require("cave");
+    var source = require('vinyl-source-stream');
+    var CleanCSS = require('clean-css');
+
+    var server = plugins.liveServer.static('./build', 4567);
+    server.config.livereload = false;
+    server.start();
+
+    penthouse({
+        url : 'http://localhost:4567/index.html',
+        css : 'build/default.css',
+        width : 600,   // viewport width
+        height : 400   // viewport height
+    }, function(err, output) {
+
+        server.stop();
+
+        var minified = new CleanCSS().minify(output).styles;
+        console.log(output)
+        var caved = cave("build/default.css", { css: minified });
+        stream = source('site.css');
+        stream.end(caved);
+        stream.pipe(plugins.minifyCss())
+            .pipe(gulp.dest('build'));
+
+        gulp.src('build/index.html')
+            .pipe(plugins.replace('default.css', 'site.css'))
+            .pipe(plugins.htmlReplace({ 'criticalcss': '<style>' + minified + '</style>' }))
+            .pipe(gulp.dest('build'));
+    });
+});*/
+
+gulp.task('default', ['copy', 'js', 'css', 'html', 'img']);
---
+++ site.js
@@ -0,0 +1,190 @@
+(function() {
+
+    var showCaseFade = 6000,
+        headerOffset = 150,
+        headerSliderHeight = 0;
+        scrollPos = 0,
+        scrollOffset = 0,
+        showcaseInterval = undefined,
+        currentImage = 0,
+        wayPoints = {},
+        wayPointObject = undefined;
+
+    var header = document.querySelector('header'),
+        headerContent = document.querySelector('#header-content'),
+        headerSlider = document.querySelector('#header-slider'),
+        banner = document.querySelector('#banner'),
+        slides = document.querySelectorAll('#header-slider > div'),
+        captions = document.querySelectorAll('#header-slider > div .caption'),
+        content = document.querySelector('#content'),
+        wrapper = document.querySelector('#wrapper'),
+        nav = document.querySelector('nav'),
+        navTop = document.querySelector('#menu'),
+        footer = document.querySelector('footer');
+
+    var debounce = function(func, wait, immediate) {
+    	var timeout;
+    	return function() {
+    		var context = this, args = arguments;
+    		var later = function() {
+    			timeout = null;
+    			if (!immediate) func.apply(context, args);
+    		};
+    		var callNow = immediate && !timeout;
+    		clearTimeout(timeout);
+    		timeout = setTimeout(later, wait);
+    		if (callNow) func.apply(context, args);
+    	};
+    };
+
+    var stretchHeader = function() {
+        headerSliderHeight = window.innerHeight;
+        [].forEach.call(document.querySelectorAll('#home, section:not(.section-quote), footer'), function(item) {
+            wayPoints[item.id] = {
+                item: item,
+                offset: item.getBoundingClientRect().top + scrollPos,
+                menu: document.querySelector('nav a[href="#' + item.id + '"]'),
+                waypoint: document.querySelector('#waypoints a[href="#' + item.id + '"]')
+            };
+        });
+    };
+
+    var currentFrame = undefined;
+    var scrollToTop = function(scrollDuration, element, offset) {
+        var scrollCount = 0, scrollMargin = 0, scrollStep, cosParameter, treshold,
+            elementOffset = (scrollPos + element.getBoundingClientRect().top + offset).toFixed(1);
+
+        if (Math.abs(scrollPos - elementOffset) < 10) return;
+        var step = function() {
+            scrollStep = Math.PI / (scrollDuration / 15);
+            cosParameter = (scrollPos - elementOffset) / 2;
+            scrollCount = scrollCount + 1;
+            scrollMargin = cosParameter - cosParameter * Math.cos(scrollCount * scrollStep);
+            treshold = (scrollPos - scrollMargin).toFixed(1);
+            if (treshold != elementOffset) {
+                window.scrollTo(0, treshold);
+                currentFrame = requestAnimationFrame(step);
+            }
+        }
+        if (!!currentFrame) cancelAnimationFrame(currentFrame);
+        currentFrame = requestAnimationFrame(step);
+    }
+
+    var runShowCase = function(first) {
+        if (slides.length > 1) {
+            requestAnimationFrame(function() {
+                if (!!first) {
+                    captions[0].classList.add('caption-up');
+                    slides[0].style.opacity = 1;
+                } else {
+                    for (var i = 0; i < slides.length; i++) {
+                        slides[i].style.opacity = 0;
+                        captions[i].classList.remove('caption-up');
+                    }
+                    currentImage = (currentImage != slides.length - 1) ? currentImage + 1 : 0;
+                    slides[currentImage].style.opacity = 1;
+                    captions[currentImage].classList.add('caption-up');
+                }
+            });
+        }
+    }
+
+    var resetShowcaseInterval = function() {
+        if (slides.length > 1) {
+            if (showcaseInterval)
+                clearInterval(showcaseInterval);
+            showcaseInterval = setInterval(function() {
+                runShowCase();
+            }, showCaseFade);
+        }
+    };
+
+    window.onload = function(e){
+        stretchHeader();
+    }
+
+    document.addEventListener("DOMContentLoaded", function() {
+        var frame, outOfBounds = false,
+            navState, timeout;
+
+        headerSliderHeight = window.innerHeight;
+        scrollPos = window.scrollY;
+
+        var setMenu = function() {
+            var limit = headerSliderHeight;
+            if (scrollPos > limit && (navState == 'up' | !!!navState)) {
+                navState = 'down';
+                navTop.classList.add('nav-up');
+            } else if (scrollPos < limit && (navState == 'down' | !!!navState)) {
+                navState = 'up';
+                navTop.classList.remove('nav-up');
+            }
+        };
+
+        stretchHeader();
+        resetShowcaseInterval();
+        runShowCase(true);
+        setMenu();
+
+        window.addEventListener('resize', function() {
+            stretchHeader();
+        }, false);
+
+        window.addEventListener('wheel', function () {
+            if (!!currentFrame) {
+                cancelAnimationFrame(currentFrame);
+                currentFrame = null;
+            }
+        });
+
+        window.addEventListener('scroll', function() {
+            // Reset showcase lest we tax the browser too much
+            resetShowcaseInterval();
+            scrollPos = window.scrollY;
+
+            if (!!frame) cancelAnimationFrame(frame);
+            frame = requestAnimationFrame(function() {
+                setMenu();
+                var limit = headerSliderHeight;
+
+                if (scrollPos <= limit) {
+                    scrollOffset = scrollPos;
+                    header.style.transform = 'translate3d(0,' + -(scrollPos / 2) + 'px,0)';
+                    headerContent.style.transform = 'translate3d(0,' + -(scrollPos * 0.2) + 'px,0)';
+                    outOfBounds = false;
+                } else if (!outOfBounds) {
+                    scrollOffset = limit;
+                    outOfBounds = true;
+                }
+
+                var cur = undefined, obj = undefined;
+                Object.keys(wayPoints).forEach(function(key) {
+                    var point = wayPoints[key];
+                    var treshold = Math.abs(point.offset - scrollPos);
+                    if (cur == undefined || treshold < cur) {
+                        cur = treshold;
+                        obj = point;
+                    }
+                });
+                
+                if (!!obj && obj != wayPointObject) {
+                    if (!!wayPointObject) {
+                        wayPointObject.waypoint.classList.remove('active');
+                        wayPointObject.menu.classList.remove('active');
+                    }
+                    wayPointObject = obj;
+                    wayPointObject.waypoint.classList.add('active');
+                    wayPointObject.menu.classList.add('active');
+                }
+            });
+        });
+
+        [].forEach.call(document.querySelectorAll('[data-scroll]'), function(item) {
+            item.addEventListener("click", function(event) {
+                event.preventDefault();
+                scrollToTop(1500, document.querySelector(item.hash || item.dataset.scroll), (item.hash || item.dataset.scroll) == "#home" ? 0 : 150);
+            });
+        });
+    });
+
+})();
